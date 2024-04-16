181b2d9621bd436f99e43b1ba4b796be1719c3f0
potomushozhenya <zhestokanov2017@yandex.ru>
2024-04-17 01:11:31 +0300

    Final

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+int main() {
+    std::ifstream in("area.in");
+    std::ofstream out("area.out");
+    int n;
+    in>>n;
+    int points[n][2];
+    for (int i = 0; i < n; ++i) {
+        in>>
+    }
+    return 0;
+}
