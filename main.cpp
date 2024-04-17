0c83caaa86521b4da091f4f2830e5d4f0962db3b
potomushozhenya <zhestokanov2017@yandex.ru>
2024-04-17 15:11:22 +0300

    Final solution

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 
 int main() {
     std::ifstream in("area.in");
@@ -7,7 +8,18 @@ int main() {
     in>>n;
     int points[n][2];
     for (int i = 0; i < n; ++i) {
-        in>>
+        in>>points[i][0]>>points[i][1];
+    }
+    long long sum=(points[n-1][1]+points[0][1])*(points[0][0]-points[n-1][0]);
+    for (int i = 0; i < n-1; ++i) {
+        sum += (points[i][1]+points[i+1][1])*(points[i+1][0]-points[i][0]);
+    }
+    if (sum<0){
+        sum*=-1;
+    }
+    out<<sum/2;
+    if ((sum&1)){
+        out<<'.'<<'5';
     }
     return 0;
 }
